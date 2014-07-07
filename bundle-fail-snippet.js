e3241527e3d57e45c8605a55f782c575fc2bac7d
Ian Horst <ian.horst@gmail.com>
2014-07-07 22:21:07 +0100

    reduced code size - server side only now

--- bundle-fail-snippet.js
+++ bundle-fail-snippet.js
@@ -1,18 +1,14 @@
 Docs = new Meteor.Collection('docs');
 
-if (Meteor.isClient) {
-  Deps.autorun(function () {
-    Meteor.call('saveDoc', {title: 'Title'}, function (error, result) {
-      console.log('Meteor.call[saveDoc]', {error: error, result: result});
-    });
-  });
-}
-
 if (Meteor.isServer) {
   Meteor.methods({
     saveDoc: function (doc) {
       console.log('Meteor.method[saveDoc]', doc);
       return Docs.upsert(doc._id, doc);
     }
+  });
+
+  Meteor.call('saveDoc', {title: 'Title'}, function (error, result) {
+    console.log('Meteor.call[saveDoc]', {error: error, result: result});
   });
 }
