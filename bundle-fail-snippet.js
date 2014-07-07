054642fdc998e9f0565c2c6c03f07f6dba931eda
Ian Horst <ian.horst@gmail.com>
2014-07-07 15:39:31 +0100

    replicate upsert bug in bundled veresion of meteor

---
+++ bundle-fail-snippet.js
@@ -0,0 +1,23 @@
+Docs = new Meteor.Collection('docs');
+
+if (Meteor.isClient) {
+  Deps.autorun(function () {
+    doc = Docs.findOne();
+
+    if (!doc) {
+      return;
+    }
+    Meteor.call('saveDoc', {title: 'Title'}, function (error, result) {
+      console.log('Meteor.call[saveDoc]', {error: error, result: result});
+    });
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.methods({
+    saveDoc: function (doc) {
+      console.log('Meteor.method[saveDoc]', doc);
+      return Docs.upsert(doc._id, doc);
+    }
+  });
+}
